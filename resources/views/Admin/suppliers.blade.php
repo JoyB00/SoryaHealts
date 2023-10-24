b678eb6d74d0d48780ad3dbb07de013df4b7a83d
NAPSTERSIHOTANG <cdsihotang12@gmail.com>
2023-10-25 06:39:17 +0700

    admin:admin, dashboard, suppliers

---
+++ resources/views/Admin/suppliers.blade.php
@@ -0,0 +1,142 @@
+@extends('admin')
+@section('content')
+<style>
+    .btn-primary.px-4{
+        border: none;
+    }
+</style>
+<div class="container-fluid">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Suppliers</h1>
+            </div>
+            <div class="col-xl-6">
+                <ol class="breadcrumb float-sm-right">
+                    <button class="btn-primary px-4" style="border-radius: 7px;" data-toggle="modal" data-target="#supplierModal"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15" height="15" viewBox="0,0,256,256">
+<g fill="#ffffff" fill-rule="evenodd" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(10.66667,10.66667)"><path d="M11,2v9h-9v2h9v9h2v-9h9v-2h-9v-9z"></path></g></g>
+</svg>Add Supplier</button>
+
+                </ol>
+            </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <table class="table table-striped border-dark text-center">
+        <tr class="">
+            <th>No</th>
+            <th>Nama</th>
+            <th>No Telepon</th>
+            <th>Email</th>
+            <th  style="width: 200px;">Action</th>
+        </tr>
+        @forelse ($pemasok as $item)
+        <tr>
+            <td>{{ $item["no"] }}</td>
+            <td>{{ $item["nama"] }}</td>
+            <td>{{ $item["noTelp"] }}</td>
+            <td>{{ $item["email"] }}</td>
+            <td class="d-flex justify-content-between" style="width: 200px;">
+                <a href="" class="btn btn-primary ms-1" style="border-radius: 10px;" data-toggle="modal" data-target="#updateModal">Update</a>
+                <a href="" class="btn btn-danger" style="border-radius: 10px;" data-toggle="modal" data-target="#deleteModal">Delete</a>
+            </td>
+        </tr>
+        @empty
+        <div class="alert alert-danger">Data Kelas masi kosong</div>
+        @endforelse
+    </table>
+</div>
+
+<!-- Modal for Update -->
+<div class="modal fade" id="updateModal" tabindex="-1" role="dialog" aria-labelledby="updateModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+                <h5 class="modal-title" id="supplierModalLabel">Update Supplier</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="supplierName">Nama Supplier:</label>
+                        <input type="text" class="form-control" id="supplierName" placeholder="Masukan Nama Supplier">
+                    </div>
+                    <div class="form-group">
+                        <label for="phoneNumber">No Telepon:</label>
+                        <input type="text" class="form-control" id="phoneNumber" placeholder="Masukan No Telepon">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" class="form-control" id="email" placeholder="Masukan Email">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save</button>
+                <button type="button" class="btn btn-danger">Reset</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal for Delete -->
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteModalLabel">Delete Supplier</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Apakah Anda yakin ingin menghapus pemasok ini?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                <button type="button" class="btn btn-danger" id="confirmDelete">Yes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Supplier Modal -->
+<div class="modal fade" id="supplierModal" tabindex="-1" role="dialog" aria-labelledby="supplierModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="supplierModalLabel">Add Supplier</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="supplierName">Nama Supplier:</label>
+                        <input type="text" class="form-control" id="supplierName" placeholder="Masukan Nama Supplier">
+                    </div>
+                    <div class="form-group">
+                        <label for="phoneNumber">No Telepon:</label>
+                        <input type="text" class="form-control" id="phoneNumber" placeholder="Masukan No Telepon">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" class="form-control" id="email" placeholder="Masukan Email">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save</button>
+                <button type="button" class="btn btn-danger">Reset</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,61 @@ Route::get('/register', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+Route::get('/dashboard', function () {
+    return view('Admin/dashboard');
+});
+
+Route::get('/suppliers', function () {
+    return view(
+        'Admin/suppliers',
+    [
+        'pemasok' =>[
+            [
+                'no'=> '1',
+                'nama'=> 'Arif Suryo',
+                'noTelp'=> '012312332',
+                'email'=> 'blabla@gmail.com',
+                'action'=> ''
+            ],
+            [
+                'no'=> '2',
+                'nama'=> 'dendy',
+                'noTelp' => '07890128192',
+                'email' => 'lololo@gmail.com',
+                'action' => ''
+            ]
+            
+        ]
+    ]
+);
+});
+
+Route::get('/customer', function () {
+    return view(
+        'Admin/customer',
+    [
+        'pelanggan' =>[
+            [
+                'no'=> '1',
+                'nama'=> 'Brader',
+                'noTelp'=> '123',
+                'email'=> 'blabla@gmail.com',
+                'action'=> ''
+            ],
+            [
+                'no'=> '2',
+                'nama'=> 'tup',
+                'noTelp' => '12434',
+                'email' => 'lololo@gmail.com',
+                'action' => 'btn-primary', 'btn-danger'
+            ]
+            
+        ]
+    ]
+);
+});
