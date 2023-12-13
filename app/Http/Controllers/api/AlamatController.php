6419d3715d4061919f240a5b1bbcddee74b1ac33
NAPSTERSIHOTANG <cdsihotang12@gmail.com>
2023-12-13 23:11:53 +0700

    addAlamatController

--- app/Http/Controllers/api/AlamatController.php
+++ app/Http/Controllers/api/AlamatController.php
@@ -2,19 +2,115 @@
 
 namespace App\Http\Controllers;
 
-use GuzzleHttp\Client;
+use App\Models\Alamat;
 use Illuminate\Http\Request;
 
 class AlamatController extends Controller
 {
     public function index()
     {
-        $client = new Client();
-        $url = "http://localhost:8000/api/alamat";
-        $response = $client->request('GET', $url);
-        $content = $response->getBody()->getContents();
-        $contentArray = json_decode($content, true);
-        $data = $contentArray['data'];
-        print_r($data);
+        try {
+            $alamat = Alamat::all();
+            return response()->json([
+                'status' => true,
+                'message' => 'Berhasil ambil data',
+                'data' => $alamat
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage(),
+                'data' => []
+            ], 400);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $alamat = Alamat::create($request->all());
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Berhasil insert data',
+                'data' => $alamat
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage(),
+                'data' => []
+            ], 400);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $alamat = Alamat::find($id);
+
+            if (!$alamat) {
+                throw new \Exception('Alamat tidak ditemukan');
+            }
+            return response()->json([
+                'status' => true,
+                'message' => 'Berhasil ambil data',
+                'data' => $alamat
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage(),
+                'data' => []
+            ], 400);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $alamat = Alamat::find($id);
+            if (!$alamat) {
+                throw new \Exception('Alamat tidak ditemukan');
+            }
+
+            $alamat->update($request->all());
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Berhasil update data',
+                'data' => $alamat
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage(),
+                'data' => []
+            ], 400);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $alamat = Alamat::find($id);
+            if (!$alamat) {
+                throw new \Exception('Berita tidak ditemukan');
+            }
+
+            $alamat->delete();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Berhasil delete data',
+                'data' => $alamat
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage(),
+                'data' => []
+            ], 400);
+        }
     }
 }
--- app/Http/Controllers/api/DetailObatController.php
+++ app/Http/Controllers/api/DetailObatController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class DetailObatController extends Controller
 {
-    //
+    public function index(){
+        $client = new Client();
+        $url = "http://localhost:8000/api/detailobat";
+        $response = $client->request('GET', $url);
+        $content = $response->getBody()->getContents();
+    }
 }
